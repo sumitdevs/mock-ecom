a1fb23a79db426c95c2eab4222ab99034ae0482c
sumit devs <mysumitdevs@gmail.com>
2025-10-31 08:26:25 +0530

    base url updated

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 export const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: BASE_URL,
 });
